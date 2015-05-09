7129ebd44312ff1fb1fccc08d5259375ef4c50d8
Ben Drucker <bvdrucker@gmail.com>
2015-05-09 10:57:50 -0700

    Init

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import lookup from 'look-up'
 import {dirname} from 'path'
 import packageRoot from 'cwd'
 import requireModule from 'require-module'
+import {sync as resolveSync} from 'resolve'
 import assert from 'assert'
 
 export default function requireRelativeMain (modulePath) {
-  const {main} = requireModule('package.json', packageRoot())
+  const basedir = packageRoot()
+  const {main} = requireModule('./package.json', basedir)
   assert(main, 'package must define a "main" entry')
-  return requireModule(modulePath, dirname(main))
+  return requireModule(modulePath, dirname(resolveSync(main, {basedir})))
 }
--- test/index.js
+++ test/index.js
@@ -1,8 +1,17 @@
 'use strict'
 
 import test from 'tape'
+import {resolve} from 'path'
 import requireRelativeMain from '../'
 
 test((t) => {
-
+  useFixtures('normal')
+  t.equal(requireRelativeMain('./other'), 'i am other')
+  useFixtures('no-main')
+  t.throws(requireRelativeMain.bind(null), /must define a "main"/)
+  t.end()
 })
+
+function useFixtures (name) {
+  process.chdir(resolve(__dirname, `./fixtures/${name}`))
+}
