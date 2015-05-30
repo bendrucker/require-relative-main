05adfe04e8f5206aadd77568eaaf7d2095d4f12b
Ben Drucker <bvdrucker@gmail.com>
2015-05-30 13:47:00 -0400

    Use find-main@2 to eliminate direct mothership, resolve deps

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 'use strict'
 
 import findMain from 'find-main'
-import {sync as mothershipSync} from 'mothership'
 import {dirname} from 'path'
 import requireModule from 'require-module'
-import {sync as resolveSync} from 'resolve'
 import assert from 'assert'
 
 export default function requireRelativeMain (modulePath, cwd) {
   cwd = cwd || process.cwd()
   const main = findMain(cwd)
   assert(main, 'package must define a "main" entry')
-  const basedir = dirname(mothershipSync(cwd, Boolean).path)
-  return requireModule(modulePath, dirname(resolveSync(main, {basedir})))
+  return requireModule(modulePath, dirname(main))
 }
