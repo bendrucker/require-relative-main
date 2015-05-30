540dad6b286e64ee50607c77ef9ecbc14a6a2bee
Ben Drucker <bvdrucker@gmail.com>
2015-05-30 13:07:37 -0400

    v1.1.2 Build

---
+++ release/index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+exports['default'] = requireRelativeMain;
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _findMain = require('find-main');
+
+var _findMain2 = _interopRequireDefault(_findMain);
+
+var _mothership = require('mothership');
+
+var _path = require('path');
+
+var _requireModule = require('require-module');
+
+var _requireModule2 = _interopRequireDefault(_requireModule);
+
+var _resolve = require('resolve');
+
+var _assert = require('assert');
+
+var _assert2 = _interopRequireDefault(_assert);
+
+'use strict';
+
+function requireRelativeMain(modulePath, cwd) {
+  cwd = cwd || process.cwd();
+  var main = _findMain2['default'](cwd);
+  _assert2['default'](main, 'package must define a "main" entry');
+  var basedir = _path.dirname(_mothership.sync(cwd, Boolean).path);
+  return _requireModule2['default'](modulePath, _path.dirname(_resolve.sync(main, { basedir: basedir })));
+}
+
+module.exports = exports['default'];
